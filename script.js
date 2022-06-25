72c714a3bc224408f59504b6b7887c01121d834d
CDwee <96454637+CDwee@users.noreply.github.com>
2022-06-25 18:28:34 -0500

    Update and rename README.md to script.js

---
+++ script.js
@@ -0,0 +1,573 @@
+// Started at 11:26 6-25-2022
+
+const express = require("express");
+const {
+  create,
+  verifyEmail,
+  resendEmailVerificationToken,
+  forgetPassword,
+  sendResetPasswordTokenStatus,
+  resetPassword,
+  signIn
+} = require("../controllers/user");
+const { isValidPassResetToken } = require("../middlewares/user");
+const {
+  userValidtor,
+  validate,
+  validatePassword,
+  signInValidator,
+} = require("../middlewares/validator");
+
+const router = express.Router();
+
+router.post("/create", userValidtor, validate, create);
+router.post("/sign-in", signInValidator, validate, signIn);
+router.post("/verify-email", verifyEmail);
+router.post("/resend-email-verification-token", resendEmailVerificationToken);
+router.post("/forget-password", forgetPassword);
+router.post(
+  "/verify-pass-reset-token",
+  isValidPassResetToken,
+  sendResetPasswordTokenStatus
+);
+router.post(
+  "/reset-password",
+  validatePassword,
+  validate,
+  isValidPassResetToken,
+  resetPassword
+);
+
+module.exports = router;
+
+const express = require("express");
+require('express-async-errors');
+const morgan = require("morgan");
+const { errorHandler } = require("./middlewares/error");
+require('dotenv').config()
+require("./db");
+const userRouter = require("./routes/user");
+
+const app = express();
+app.use(express.json());
+app.use(morgan('dev'));
+app.use("/api/user", userRouter);
+
+app.use(errorHandler)
+
+// app.post(
+//   "/sign-in",
+//   (req, res, next) => {
+//     const { email, password } = req.body;
+//     if (!email || !password)
+//       return res.json({ error: "email/password missing!" });
+//     next();
+//   },
+//   (req, res) => {
+//     res.send("<h1>Hello I am about page</h1>");
+//   }
+// );
+
+app.listen(8000, () => {
+  console.log("the port is listening on port 8000");
+});
+
+exports.errorHandler = (err, req, res, next) => {
+    res.status(500).json({ error: err.message || err });
+}
+
+const { check, validationResult } = require("express-validator");
+
+exports.userValidtor = [
+  check("name").trim().not().isEmpty().withMessage("Name is missing!"),
+  check("email").normalizeEmail().isEmail().withMessage("Email is invalid!"),
+  check("password")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("Password is missing!")
+    .isLength({ min: 8, max: 20 })
+    .withMessage("Password must be 8 to 20 characters long!"),
+];
+
+exports.validatePassword = [
+  check("newPassword")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("Password is missing!")
+    .isLength({ min: 8, max: 20 })
+    .withMessage("Password must be 8 to 20 characters long!"),
+];
+
+exports.signInValidator = [
+  check("email").normalizeEmail().isEmail().withMessage("Email is invalid!"),
+  check("password").trim().not().isEmpty().withMessage("Password is missing!"),
+];
+
+exports.validate = (req, res, next) => {
+  const error = validationResult(req).array();
+  if (error.length) {
+    return res.json({ error: error[0].msg });
+  }
+
+  next();
+};
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isVerified: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+
+  next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  const result = await bcrypt.compare(password, this.password);
+  return result;
+};
+
+module.exports = mongoose.model("User", userSchema);
+
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const EmailVerificationToken = require("../models/emailVerificationToken");
+const PasswordResetToken = require("../models/passwordResetToken");
+const { isValidObjectId } = require("mongoose");
+const { generateOTP, generateMailTransporter } = require("../utils/mail");
+const { sendError, generateRandomByte } = require("../utils/helper");
+
+exports.create = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const oldUser = await User.findOne({ email });
+
+  if (oldUser) return sendError(res, "This email is already in use!");
+
+  const newUser = new User({ name, email, password });
+  await newUser.save();
+
+  // generate 6 digit otp
+  let OTP = generateOTP();
+
+  // store otp inside our db
+  const newEmailVerificationToken = new EmailVerificationToken({
+    owner: newUser._id,
+    token: OTP,
+  });
+
+  await newEmailVerificationToken.save();
+
+  // send that otp to our user
+
+  var transport = generateMailTransporter();
+
+  transport.sendMail({
+    from: "verification@reviewapp.com",
+    to: newUser.email,
+    subject: "Email Verification",
+    html: `
+      <p>Your verification OTP</p>
+      <h1>${OTP}</h1>
+
+    `,
+  });
+
+  res.status(201).json({
+    message:
+      "Please verify your email. OTP has been sent to your email accont!",
+  });
+};
+
+exports.verifyEmail = async (req, res) => {
+  const { userId, OTP } = req.body;
+
+  if (!isValidObjectId(userId)) return res.json({ error: "Invalid user!" });
+
+  const user = await User.findById(userId);
+  if (!user) return sendError(res, "user not found!", 404);
+
+  if (user.isVerified) return sendError(res, "user is already verified!");
+
+  const token = await EmailVerificationToken.findOne({ owner: userId });
+  if (!token) return sendError(res, "token not found!");
+
+  const isMatched = await token.compareToken(OTP);
+  if (!isMatched) return sendError(res, "Please submit a valid OTP!");
+
+  user.isVerified = true;
+  await user.save();
+
+  await EmailVerificationToken.findByIdAndDelete(token._id);
+
+  var transport = generateMailTransporter();
+
+  transport.sendMail({
+    from: "verification@reviewapp.com",
+    to: user.email,
+    subject: "Welcome Email",
+    html: "<h1>Welcome to our app and thanks for choosing us.</h1>",
+  });
+  res.json({ message: "Your email is verified." });
+};
+
+exports.resendEmailVerificationToken = async (req, res) => {
+  const { userId } = req.body;
+
+  const user = await User.findById(userId);
+  if (!user) return sendError(res, "user not found!");
+
+  if (user.isVerified)
+    return sendError(res, "This email id is already verified!");
+
+  const alreadyHasToken = await EmailVerificationToken.findOne({
+    owner: userId,
+  });
+  if (alreadyHasToken)
+    return sendError(
+      res,
+      "Only after one hour you can request for another token!"
+    );
+
+  // generate 6 digit otp
+  let OTP = generateOTP();
+
+  // store otp inside our db
+  const newEmailVerificationToken = new EmailVerificationToken({
+    owner: user._id,
+    token: OTP,
+  });
+
+  await newEmailVerificationToken.save();
+
+  // send that otp to our user
+
+  var transport = generateMailTransporter();
+
+  transport.sendMail({
+    from: "verification@reviewapp.com",
+    to: user.email,
+    subject: "Email Verification",
+    html: `
+      <p>Your verification OTP</p>
+      <h1>${OTP}</h1>
+
+    `,
+  });
+
+  res.json({
+    message: "New OTP has been sent to your registered email accout.",
+  });
+};
+
+exports.forgetPassword = async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) return sendError(res, "email is missing!");
+
+  const user = await User.findOne({ email });
+  if (!user) return sendError(res, "User not found!", 404);
+
+  const alreadyHasToken = await PasswordResetToken.findOne({ owner: user._id });
+  if (alreadyHasToken)
+    return sendError(
+      res,
+      "Only after one hour you can request for another token!"
+    );
+
+  const token = await generateRandomByte();
+  const newPasswordResetToken = await PasswordResetToken({
+    owner: user._id,
+    token,
+  });
+  await newPasswordResetToken.save();
+
+  const resetPasswordUrl = `http://localhost:3000/reset-password?token=${token}&id=${user._id}`;
+
+  const transport = generateMailTransporter();
+
+  transport.sendMail({
+    from: "security@reviewapp.com",
+    to: user.email,
+    subject: "Reset Password Link",
+    html: `
+      <p>Click here to reset password</p>
+      <a href='${resetPasswordUrl}'>Change Password</a>
+
+    `,
+  });
+
+  res.json({ message: "Link sent to your email!" });
+};
+
+exports.sendResetPasswordTokenStatus = (req, res) => {
+  res.json({ valid: true });
+};
+
+exports.resetPassword = async (req, res) => {
+  const { newPassword, userId } = req.body;
+
+  const user = await User.findById(userId);
+  const matched = await user.comparePassword(newPassword);
+  if (matched)
+    return sendError(
+      res,
+      "The new password must be different from the old one!"
+    );
+
+  user.password = newPassword;
+  await user.save();
+
+  await PasswordResetToken.findByIdAndDelete(req.resetToken._id);
+
+  const transport = generateMailTransporter();
+
+  transport.sendMail({
+    from: "security@reviewapp.com",
+    to: user.email,
+    subject: "Password Reset Successfully",
+    html: `
+      <h1>Password Reset Successfully</h1>
+      <p>Now you can use your new password.</p>
+
+    `,
+  });
+
+  res.json({
+    message: "Password reset successfully, now you can use new password.",
+  });
+};
+
+exports.signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if(!user) return sendError(res, 'Email/Password missmatch!');
+  
+    const matched = await user.comparePassword(password); 
+    if(!matched) return sendError(res, 'Email/Password missmatch!');
+  
+    const { _id, name } = user;
+  
+    const jwtToken = jwt.sign({ userId: _id }, process.env.JWT_SECRET);
+  
+    res.json({ user: { id: _id, name, email, token: jwtToken } });
+};
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("db is connected!");
+  })
+  .catch((ex) => {
+    console.log("db connection failed: ", ex);
+  });
+
+const nodemailer = require("nodemailer");
+
+exports.generateOTP = (otp_length = 6) => {
+  let OTP = "";
+  for (let i = 1; i <= otp_length; i++) {
+    const randomVal = Math.round(Math.random() * 9);
+    OTP += randomVal;
+  }
+
+  return OTP;
+};
+
+exports.generateMailTransporter = () =>
+nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.MAIL_TRAP_USER,
+    pass: process.env.MAIL_TRAP_PASS,
+  },
+});
+
+JWT_SECRET=hotDoggyDog
+MONGO_URI=mongodb+srv://carter:BigfeetBigtoes1@cluster0.dnpiw.mongodb.net/?retryWrites=true&w=majority
+MAIL_TRAP_USER=0052cc33e23149
+MAIL_TRAP_PASS=4d10e4518c5f1c
+
+import React, { useEffect, useRef, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { resendEmailVerificationToken, verifyUserEmail } from "../../api/auth";
+import { useAuth, useNotification } from "../../hooks";
+
+import { commonModalClasses } from "../../utils/theme";
+import Container from "../Container";
+import FormContainer from "../form/FormContainer";
+import Submit from "../form/Submit";
+import Title from "../form/Title";
+
+const OTP_LENGTH = 6;
+
+const isValidOTP = (otp) => {
+  let valid = false;
+
+  for (let val of otp) {
+    valid = !isNaN(parseInt(val));
+    if (!valid) break;
+  }
+
+  return valid;
+};
+
+export default function EmailVerification() {
+  const [otp, setOtp] = useState(new Array(OTP_LENGTH).fill(""));
+  const [activeOtpIndex, setActiveOtpIndex] = useState(0);
+
+  const { isAuth, authInfo } = useAuth();
+  const { isLoggedIn, profile } = authInfo;
+  const isVerified = profile?.isVerified;
+
+  const inputRef = useRef();
+  const { updateNotification } = useNotification();
+
+  const { state } = useLocation();
+  const user = state?.user;
+
+  const navigate = useNavigate();
+
+  const focusNextInputField = (index) => {
+    setActiveOtpIndex(index + 1);
+  };
+
+  const focusPrevInputField = (index) => {
+    let nextIndex;
+    const diff = index - 1;
+    nextIndex = diff !== 0 ? diff : 0;
+
+    setActiveOtpIndex(nextIndex);
+  };
+
+  const handleOtpChange = ({ target }, index) => {
+    const { value } = target;
+    const newOtp = [...otp];
+    newOtp[index] = value.substring(value.length - 1, value.length);
+
+    if (!value) focusPrevInputField(index);
+    else focusNextInputField(index);
+
+    setOtp([...newOtp]);
+  };
+
+  const handleOTPResend = async () => {
+    const { error, message } = await resendEmailVerificationToken(user.id);
+
+    if (error) return updateNotification("error", error);
+
+    updateNotification("success", message);
+  };
+
+  const handleKeyDown = ({ key }, index) => {
+    if (key === "Backspace") {
+      focusPrevInputField(index);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!isValidOTP(otp)) return updateNotification("error", "invalid OTP");
+
+    // submit otp
+    const {
+      error,
+      message,
+      user: userResponse,
+    } = await verifyUserEmail({
+      OTP: otp.join(""),
+      userId: user.id,
+    });
+    if (error) return updateNotification("error", error);
+
+    updateNotification("success", message);
+    localStorage.setItem("auth-token", userResponse.token);
+    isAuth();
+  };
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [activeOtpIndex]);
+
+  useEffect(() => {
+    if (!user) navigate("/not-found");
+    if (isLoggedIn && isVerified) navigate("/");
+  }, [user, isLoggedIn, isVerified]);
+
+  // if(!user) return null;
+
+  return (
+    <FormContainer>
+      <Container>
+        <form onSubmit={handleSubmit} className={commonModalClasses}>
+          <div>
+            <Title>Please enter the OTP to verify your account</Title>
+            <p className="text-center dark:text-dark-subtle text-light-subtle">
+              OTP has been sent to your email
+            </p>
+          </div>
+
+          <div className="flex justify-center items-center space-x-4">
+            {otp.map((_, index) => {
+              return (
+                <input
+                  ref={activeOtpIndex === index ? inputRef : null}
+                  key={index}
+                  type="number"
+                  value={otp[index] || ""}
+                  onChange={(e) => handleOtpChange(e, index)}
+                  onKeyDown={(e) => handleKeyDown(e, index)}
+                  className="w-12 h-12 border-2 dark:border-dark-subtle border-light-subtle dark:focus:border-white focus:border-primary rounded bg-transparent outline-none text-center dark:text-white text-primary font-semibold text-xl spin-button-none"
+                />
+              );
+            })}
+          </div>
+
+          <div>
+            <Submit value="Verify Account" />
+            <button
+              onClick={handleOTPResend}
+              type="button"
+              className="dark:text-white text-blue-500 font-semibold hover:underline mt-2"
+            >
+              I don't have OTP
+            </button>
+          </div>
+        </form>
+      </Container>
+    </FormContainer>
+  );
+}
+
+// Ended at 6:28 6-25-2022
